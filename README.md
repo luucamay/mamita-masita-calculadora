d8514a32ffae06c8bf13a776c7ca27c897a068d7
Lupe <lupemaydana@gmail.com>
2023-04-25 22:08:00 -0400

    feat: make web app installable

--- README.md
+++ README.md
@@ -6,4 +6,9 @@ Calculadora de pesos y gramos para masa madre
 
 - crete a component for ingredient in new recipe with its specific styles
 - transform in a PWA
-- how to locally save values until internet connection
+- how to locally save values until internet connection indexdb or local storage
+
+## Resources
+
+- how to make your app intalable https://dev.to/codesphere/how-and-why-to-create-a-progressive-web-app-41kb
+- advanced codelab to make your app instalable https://web.dev/codelab-make-installable/
--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+
+// Window Load Event
+window.addEventListener('load', () => {
+  registerWorker();
+});
+
+// Register Worker Function
+async function registerWorker() {
+  if ('serviceWorker' in navigator) {
+    try {
+      await navigator
+        .serviceWorker
+        .register('serviceworker.js');
+    }
+    catch (e) {
+      console.log('service worker not registered');
+    }
+  }
+}
+
+/* Put code here */
 const addIngredientBtn = document.getElementById("addIngredient");
 const addIngredient = () => {
   // create new ingredient component
