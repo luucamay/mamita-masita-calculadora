24eecbf6cbc0b30804a1c466db015f35dd77a671
Lupe <lupemaydana@gmail.com>
2023-04-26 18:38:35 -0400

    feat: update service worker assets

--- serviceworker.js
+++ serviceworker.js
@@ -1,19 +1,24 @@
-var staticCacheName = "hello-pwa";
+const staticCacheName = "mamita-masita-cache";
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js"
+]
+// self is the self worker itself
 
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]);
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      cache.addAll(assets)
     })
-  );
-});
+  )
+})
 
-self.addEventListener("fetch", function (event) {
-  console.log(event.request.url);
-
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
     })
-  );
-});
+  )
+})
