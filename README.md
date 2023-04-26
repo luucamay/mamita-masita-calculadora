3cc580c5bc7dfed73badcc34422d89dbb5485715
Lupe <lupemaydana@gmail.com>
2023-04-26 19:48:53 -0400

    save recipe function to create a new recipe

--- README.md
+++ README.md
@@ -4,19 +4,28 @@ Calculadora de pesos y gramos para masa madre
 
 ## TO DO
 
+- Create the save function
+- remove ingredient
+- drag ingredient to change display order
+- Add percentage symbol next to number in the input value for percentage
+
+## TO DO v2
+
 - crete a component for ingredient in new recipe with its specific styles
-- how to locally save values until internet connection indexdb or local storage
-- check what is available ofline
+- sort ingredients from heavier to lighter?
+- how to locally save values until internet connection indexdb or local storage (future version)
 
 ## Done
 
 - transform in a PWA
+- check it is available ofline
 
 ## Learned
 
 - In order to create a PWA you need 2 main files:
   - manifest.json
   - serviceworker.js
+- It is key that your service worker caches all the assets you are interested in
 
 ## Resources
 
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 // Window Load Event
 window.addEventListener('load', () => {
-  registerWorker();
+  // registerWorker();
 });
 
 // Register Worker Function
@@ -14,19 +14,51 @@ const registerWorker = () => {
   }
 }
 
-/* Put code here */
-const addIngredientBtn = document.getElementById("addIngredient");
+/* Add a new ingredient */
+const addIngredientBtn = document.querySelector("#addIngredient")
 const addIngredient = () => {
   // create new ingredient component
-  const ingredient = document.createElement("div");
-  const ingPercentage = document.createElement("input");
-  const ingName = document.createElement("input");
-  ingPercentage.placeholder = "0%";
-  ingName.placeholder = "nombre de ingrediente";
-  ingredient.appendChild(ingPercentage);
-  ingredient.appendChild(ingName);
-  const ingredients = document.getElementById("ingredients");
-  ingredients.appendChild(ingredient);
+  const ingredient = document.createElement("div")
+  const ingPercentage = document.createElement("input")
+  const symbol = document.createElement("p")
+  const ingName = document.createElement("input")
+  symbol.textContent = '%'
+  ingredient.classList.add('ingredient')
+  ingName.placeholder = "nombre de ingrediente"
+  ingPercentage.placeholder = "0"
+  ingName.classList.add('name')
+  ingPercentage.classList.add('percentage')
+  ingredient.appendChild(ingPercentage)
+  ingredient.appendChild(symbol)
+  ingredient.appendChild(ingName)
+  const ingredients = document.getElementById("ingredients")
+  ingredients.appendChild(ingredient)
 
 }
-addIngredientBtn.addEventListener("click", addIngredient);
+addIngredientBtn.addEventListener("click", addIngredient)
+
+const recipeList = []
+/* Save a recipe */
+const saveRecipeBtn = document.querySelector("#saveRecipe")
+const saveRecipe = () => {
+  /*get values from inputs
+    name recipe
+    ingredients name, percentage
+   */
+  const newRecipe = {}
+  newRecipe['name'] = document.querySelector("#recipeName").value
+  newRecipe['ingredientList'] = []
+
+  const ingredients = document.querySelector('#ingredients')
+  for (const ingredientDiv of ingredients.children) {
+    const ingredient = {}
+    ingredient['name'] = ingredientDiv.querySelector('.name').value
+    ingredient['percentage'] = ingredientDiv.querySelector('.percentage').value
+
+    newRecipe['ingredientList'].push(ingredient)
+  }
+  recipeList.push(newRecipe)
+  //console.log({ recipeList })
+
+}
+saveRecipeBtn.addEventListener("click", saveRecipe);
